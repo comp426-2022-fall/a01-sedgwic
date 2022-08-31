83e0d3fb7c0fb0c0ef583cb3de0b5db78412b814
sedgwic <sedgwic@email.unc.edu>
2022-08-31 19:45:46 -0400

    fixed console log port error

--- server.js
+++ server.js
@@ -20,10 +20,10 @@ const port = args.port || 3000;
 // The function must read a file located at `./public/index.html` and do some stuff with it.
 // The stuff that should be inside this function is all below.
 fs.readFile('./public/index.html', 'utf8', (err, data) =>  {
-  if (err) {
-    console.log(err);
-    return;
-  }
+    if (err) {
+      console.log(err);
+      return;
+    }
 
 // If there is an error, put it on the console error and return.
 // Do not be nice about exiting.
@@ -38,20 +38,20 @@ fs.readFile('./public/index.html', 'utf8', (err, data) =>  {
 // 1. status code 200,
 // 2. set a header with content type `text/html`, and
 // 3. end with the data that you are reading in from ./public/index.html.
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
-  res.end(data);
-});
+    const server = http.createServer((req, res) => {
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/html");
+      res.end(data);
+    });
 
 
 
 
 // Start the `server` const listening on the port defined by argument in your `port` const.
 // Put the exact message `Server listening on port ${port}` on the console log.
-server.listen((port, err) => {
-  return console.log('Server Listening on port ${port}');
-}); 
-})
+    server.listen(port, ()  => {
+      return console.log(`Server Listening on port ${port}`);
+    }); 
+});
 // That's it! You're all done!
 
